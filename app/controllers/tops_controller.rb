dc6c6a7e150f0dfc8a2379e347783629a1172d0a
EC2 Default User <ec2-user@ip-172-31-35-48.ec2.internal>
2019-12-26 07:43:04 +0000

    first commit

---
+++ app/controllers/tops_controller.rb
@@ -0,0 +1,7 @@
+class TopsController < ApplicationController
+  
+  def index
+    @top = Top.find(1)
+  end
+  
+end
---
+++ db/migrate/20191226071544_create_tops.rb
@@ -0,0 +1,11 @@
+class CreateTops < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tops do |t|
+      
+      t.text        :text
+      t.text        :image
+
+      t.timestamps
+    end
+  end
+end
