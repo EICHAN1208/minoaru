f411fa0f26c8d0cee0db37f32149ad1989e22ebc
EC2 Default User <ec2-user@ip-172-31-35-48.ec2.internal>
2020-01-07 10:00:49 +0000

    seedsファイルを編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Tops.create!(
-   text: 'テスト',
-)
+Top.create(text: 'テスト', image: 'テスト')
