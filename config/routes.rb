a4271ad6d7d8cc9a59493a3a3f4fdef4cbcfc059
EC2 Default User <ec2-user@ip-172-31-35-48.ec2.internal>
2020-01-07 08:44:12 +0000

    commit5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#index'
-  get 'tops' => 'tops#index'
   get 'aquestions' => 'aquestions#index'
   get 'aanswers/:id' => 'aanswers#index' #それぞれの問題の解答ボタンが押されると問題のidに応じたリンクへ飛ぶ
   get 'bquestions' => 'bquestions#index'
