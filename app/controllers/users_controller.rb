db6d8a52cc6c973f189b4f3e3dc947db047f51e3
EC2 Default User <ec2-user@ip-172-31-35-48.ec2.internal>
2020-01-11 14:02:53 +0000

    マイページの順番の変更とページネーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   def show
     @nickname = current_user.nickname
-    @kakomons = current_user.kakomons  #アソシエーションによりできるようになった箇所
+    @kakomons = current_user.kakomons.order("created_at DESC").page(params[:page]).per(5)  #（前半部分）アソシエーションによりできるようになった箇所
   end
   
 end
