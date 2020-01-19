fbdb64f50bb0d5953e2c3e504596bf07215788d2
EC2 Default User <ec2-user@ip-172-31-35-48.ec2.internal>
2020-01-19 09:44:01 +0000

    答えのページに適切に遷移するようにした

--- app/controllers/aanswers_controller.rb
+++ app/controllers/aanswers_controller.rb
@@ -2,7 +2,6 @@ class AanswersController < ApplicationController
   
   def index
     @aanswer = Aanswer.find(params[:id]) #ここは解答ボタンが押されて飛んでくるためidはparamsに入る
-    redirect_to controller: 'tops', action: 'index' unless user_signed_in?
   end
   
 end
--- app/controllers/banswers_controller.rb
+++ app/controllers/banswers_controller.rb
@@ -2,7 +2,6 @@ class BanswersController < ApplicationController
   
   def index
     @banswer = Banswer.find(params[:id]) #ここは解答ボタンが押されて飛んでくるためidはparamsに入る
-    redirect_to controller: 'tops', action: 'index' unless user_signed_in?
   end
   
 end
