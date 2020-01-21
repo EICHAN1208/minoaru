feb31e772ec21883c23ccfcc038fbe8ed5b96e84
EC2 Default User <ec2-user@ip-172-31-35-48.ec2.internal>
2020-01-21 03:18:14 +0000

    コメント機能の改善１

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   
   def show
-    @nickname = current_user.nickname
-    @kakomons = current_user.kakomons.order("created_at DESC").page(params[:page]).per(5)  #（前半部分）アソシエーションによりできるようになった箇所
+    user = User.find(params[:id])   #userとういう変数をつくることでそれぞれのユーザーページに飛ぶようにできる
+    @nickname = user.nickname
+    @kakomons = user.kakomons.order("created_at DESC").page(params[:page]).per(5)  #（前半部分）アソシエーションによりできるようになった箇所
   end
   
 end
