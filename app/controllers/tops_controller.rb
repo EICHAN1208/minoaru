98b427d42fb745593a85d44797022d7ec37e0b8a
EC2 Default User <ec2-user@ip-172-31-35-48.ec2.internal>
2020-01-21 03:32:44 +0000

    トップコントローラーとトップ画面の編集

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,9 +2,7 @@ class TopsController < ApplicationController
   layout 'tops' #layoutsのtops.html.erbを適用させる（headerのメニューを表示したくないため）
   
   def index
-    if user_signed_in?
-      redirect_to '/users/current_user.id'
-    end
+  
   end
   
 end
