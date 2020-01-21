0176ed34032bdfdcb6bc7fbbf1bacbdc30bdf517
EC2 Default User <ec2-user@ip-172-31-35-48.ec2.internal>
2020-01-21 13:09:10 +0000

    ログイン後に消防法問題の表示

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,7 +2,11 @@ class TopsController < ApplicationController
   layout 'tops' #layoutsのtops.html.erbを適用させる（headerのメニューを表示したくないため）
   
   def index
-  
+    #ログインした後に、消防法の問題を表示させる
+    if user_signed_in?
+      redirect_to controller: 'aquestions', action: 'index'
+    else
+    end
   end
   
 end
