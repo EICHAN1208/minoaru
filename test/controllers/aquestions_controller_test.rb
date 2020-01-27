fe24ffed782bf2962f4e23bbd3f59c532aad0234
EC2 Default User <ec2-user@ip-172-31-35-48.ec2.internal>
2020-01-27 11:50:45 +0000

    guardによるテストの自動化

--- test/controllers/aquestions_controller_test.rb
+++ test/controllers/aquestions_controller_test.rb
@@ -4,4 +4,10 @@ class AquestionsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+  test "should get index" do
+    get aquestions_url
+    assert_response :success
+  end
+  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require "minitest/reporters"   #red や green を表示できるようにする
+Minitest::Reporters.use!      #red や green を表示できるようにする
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
